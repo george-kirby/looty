1c327cbd3cb23ad860228e2be2273fd28afd25c0
george-kirby <georgeskirby@btinternet.com>
2022-10-31 16:03:32 +0000

    create data

---
+++ data/loots.js
@@ -0,0 +1,13 @@
+const loots = [
+    {
+        id: 1,
+        name: "Emerald Isle",
+        slug: "emerald-isle",
+        dangers: ["snakes", "hurricanes"],
+        distance: 150,
+        lootValue: 300,
+        description: "Most of the time this island appears sunny and welcoming. Don't get lulled into complacency - watch out for the snakes!"
+    }
+]
+
+export default loots
