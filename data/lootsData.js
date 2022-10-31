ebcc20d59b8f95b7ca078f1b39586f258605da07
george-kirby <georgeskirby@btinternet.com>
2022-10-31 18:14:01 +0000

    rename lootsData.js

---
+++ data/lootsData.js
@@ -0,0 +1,53 @@
+const loots = [
+    {
+        id: 1,
+        title: "Emerald Isle",
+        slug: "emerald-isle",
+        dangers: ["snakes", "hurricanes"],
+        distance: 150,
+        value: 300,
+        description: "Most of the time this island appears sunny and welcoming. Don't get lulled into complacency - watch out for the snakes!",
+        image: {
+            src: "/images/islands/EmeraldIsle.png",
+            alt: "Emerald Isle"
+        }
+    },
+    {
+        id: 2,
+        title: "Jade Isle",
+        slug: "jade-isle",
+        dangers: ["unknown"],
+        distance: 500,
+        value: 10,
+        description: "Why even bother? So far away for such little wealth! Unless there's more than meets the eye...",
+    },
+    {
+        id: 3,
+        title: "Jade Isle",
+        slug: "jade-isle",
+        dangers: ["unknown"],
+        distance: 500,
+        value: 10,
+        description: "Why even bother? So far away for such little wealth! Unless there's more than meets the eye..."
+    },
+    {
+        id: 4,
+        title: "Jade Isle",
+        slug: "jade-isle",
+        dangers: ["unknown"],
+        distance: 500,
+        value: 10,
+        description: "Why even bother? So far away for such little wealth! Unless there's more than meets the eye..."
+    },
+    {
+        id: 5,
+        title: "Jade Isle",
+        slug: "jade-isle",
+        dangers: ["unknown"],
+        distance: 500,
+        value: 10,
+        description: "Why even bother? So far away for such little wealth! Unless there's more than meets the eye..."
+    },
+]
+
+export default loots
