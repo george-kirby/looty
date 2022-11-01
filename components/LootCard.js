73c8ca0cee770a7ba74e8e94fc6531a2927fae17
george-kirby <georgeskirby@btinternet.com>
2022-11-01 11:03:24 +0000

    add first filter

--- components/LootCard.js
+++ components/LootCard.js
@@ -1,22 +1,30 @@
+import { Card } from '@mui/material';
 import Image from 'next/image';
 
 export default function LootCard( { loot } ) {
     const image = loot.image || { src: `/images/islands/EmeraldIsle.png`, alt: "Emerald Isle" }
 
     return (
-        <div>
-            <Image
+        <Card variant="outlined">
+            {/* <Image
               priority
               src={image.src}
               height={20}
               width={20}
               alt={image.alt}
-            />
+            /> */}
             <h3>{loot.title}</h3>
             <ul>
                 <li>{loot.distance} km</li>
                 <li>{loot.value} dubloons</li>
             </ul>
-        </div>
+            <ul className="dangers-list">
+            {loot.dangers.map((danger, index) => {
+                return (
+                  <li key={index}>{danger}</li>
+                )
+              })}
+            </ul>
+        </Card>
     )
 }
--- pages/loots/index.js
+++ pages/loots/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react"
 import { Checkbox, FormControlLabel } from '@mui/material';
 import Layout from '../../components/layout';
 import LootCardsList from '../../components/LootCardsList';
@@ -13,11 +14,39 @@ export async function getStaticProps() {
   }
 
 export default function LootsIndex( { allLootsData }) {
+    const [unwantedDangers, setUnwantedDangers] = useState([]);
+    const [filteredLoots, setFilteredLoots] = useState(allLootsData);
+
+    useEffect(() => {
+        const newFilteredLoots = allLootsData.filter(loot => {
+            return !loot.dangers.some(danger => unwantedDangers.includes(danger))
+        })
+        setFilteredLoots(newFilteredLoots)
+        }, [unwantedDangers]);
+
+    const toggleSnakes = (event, value) => {
+        if (value) {
+            setUnwantedDangers([...unwantedDangers, "snakes"])
+        } else {
+            const snakesAcceptable = [...unwantedDangers]
+            const index = snakesAcceptable.indexOf("snakes");
+                if (index > -1) { 
+                    snakesAcceptable.splice(index, 1);
+                }
+            setUnwantedDangers(snakesAcceptable)
+        }
+    }
+
+    // update loots accordingly
+    // const applyChanges = () => {
+    //     setFilteredLoots(filters)
+    // }
+
     return (
         <Layout>
             <main>
-            <FormControlLabel control={<Checkbox />} label="Label" />
-            <LootCardsList loots={allLootsData} />
+            <FormControlLabel control={<Checkbox />} label="no snakes pls" onChange={toggleSnakes} />
+            <LootCardsList loots={filteredLoots} />
 
             </main>
         </Layout>
